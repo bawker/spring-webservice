e21c3bac25fa2d48f30b73a3d07db76e3449c115
bbaker87 <friend383838@gmail.com>
2019-07-02 15:26:11 +0900

    set1, set2 Profile 설정

--- src/main/java/com/bbawker/webservice/web/WebRestController.java
+++ src/main/java/com/bbawker/webservice/web/WebRestController.java
@@ -5,10 +5,13 @@ import com.bbawker.webservice.domain.posts.PostsRepository;
 import com.bbawker.webservice.dto.posts.PostsSaveRequestDto;
 import com.bbawker.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 
 /**
@@ -34,6 +37,7 @@ public class WebRestController {
 
     //private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -48,5 +52,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
