a8a13743d2e4a40ee9978ba43595a65e308655d0
bbaker87 <friend383838@gmail.com>
2019-10-16 15:39:45 +0900

    템플릿 변경

--- src/main/java/com/bbawker/webservice/security/WebMvcConfig.java
+++ src/main/java/com/bbawker/webservice/security/WebMvcConfig.java
@@ -1,23 +1,23 @@
-package com.bbawker.webservice.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-
-/**
- * 머스터치에선 바로 Csrf를 사용할 수 없기 때문에 추가
- */
-@Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter
-{
-    @Autowired
-    CsrfInterceptor csrfInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry)
-    {
-        registry.addInterceptor(csrfInterceptor);
-    }
-}
+//package com.bbawker.webservice.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//
+///**
+// * 머스터치에선 바로 Csrf를 사용할 수 없기 때문에 추가
+// */
+//@Configuration
+//public class WebMvcConfig extends WebMvcConfigurerAdapter
+//{
+//    @Autowired
+//    CsrfInterceptor csrfInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry)
+//    {
+//        registry.addInterceptor(csrfInterceptor);
+//    }
+//}
